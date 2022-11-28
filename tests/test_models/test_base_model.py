1f3f0db6e6206b728a8d6bc0a9967c61ccc9c1a9
Geoffy1 <geojogu@gmail.com>
2022-11-27 16:23:09 -0800

    edits of test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -108,6 +108,7 @@ class TestBaseModel(unittest.TestCase):
         """test the _str method
         that returns (str) a descriptor for BM Class
         """
+        BM = BaseModel()
         String = str(self.BM)
         BM.id = '[{}] ({}) {}'.format(self.BM._class_._name_,
                                      self.BM.id, self.BM.__dict__)
@@ -124,11 +125,6 @@ class TestBaseModel(unittest.TestCase):
         BM1 = BaseModel()
         BM2 = BaseModel()
         self.assertNotEqual(BM1.id, BM2.id)
-
-    def test_id__type__str(self):
-        """Checks if id is a string"""
-        BM = BaseModel()
-        self.assertEqual(type(self.BM.id), str)
 
     def test_created_time(self):
         """
